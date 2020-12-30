366ab089d53b09c94d6062edae632de8959be142
ayaayato <73512438+ayaayato@users.noreply.github.com>
2020-12-31 05:54:44 +0900

    送信機能追加

---
+++ app/Mail/Contact.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Contact extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($inputs)
+    {
+        //
+        $this->contact = $inputs;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('【問い合わせ】'.$this->contact['category'])
+        ->view('contact.mail')
+        ->with([
+            'email' => $this->contact['email'],
+            'category' => $this->contact['category'],
+            'body'  => $this->contact['body'],
+        ]);
+        
+    }
+}
---
+++ resources/views/contact/sent.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.common')
+
+@section('content')
+
+<h1>お問い合わせを受け付けました。</h1>
+<br>
+<a href="/">
+<button class="btn" type="button">別のお問い合わせを送る</button>
+</a>
+<br>
+<a href="https://hydrangea1008.herokuapp.com/">
+<button class="btn" type="button">Hydrangeaへ戻る</button>
+</a>
+
+@endsection
