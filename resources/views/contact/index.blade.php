21202c8cf991c3cbe8170b4b98b0b2061e43d95a
ayaayato <73512438+ayaayato@users.noreply.github.com>
2021-01-02 02:13:58 +0900

    種類修正

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -21,7 +21,7 @@
          ?>
 
         <select class="inputs" name="category">
-          <option value="未選択">選択してください</option>
+          <option value="">選択してください</option>
           <?php
           foreach ($types as $type){
             echo "<option value='{$type}'>{$type}</option>";
