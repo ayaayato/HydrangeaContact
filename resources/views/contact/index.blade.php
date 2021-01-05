cb4a79b2a4d462e43436e7a67fa3acad3000759c
ayaayato <73512438+ayaayato@users.noreply.github.com>
2021-01-06 07:19:37 +0900

    レイアウト修正

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -9,13 +9,13 @@
       <form method="post" action="{{ route('contact.confirm') }}">
       @csrf
         <div class="form-item">
-        <h4>メールアドレス （必須）</h4>
-        <input class="inputs" type="text" name="email">
+        <h4>メールアドレス</h4>
+        <input class="inputs email-inp" type="text" name="email">
         </div>
         <br>
 
         <div class="form-item">
-        <h4>お問い合わせの種類 （必須）</h4>
+        <h4>お問い合わせの種類</h4>
         <?php 
           $types = array('Hydrangeaに関するお問い合わせ', 'ご意見・ご要望', 'エラー・バグの報告', '不適切な投稿の報告', '開発者へのご相談・ご依頼', 'その他');
          ?>
@@ -32,9 +32,10 @@
 
         <br>
         <div class="form-item">
-        <h4>内容 （必須）</h4>
+        <h4>内容</h4>
         <textarea class="inputs" name="body"></textarea>
         </div>
+        <h4 class="danger">入力内容は全て必須です</h4>
         <br>
       <div>
         <input class="btn" type="submit" value="送信">
